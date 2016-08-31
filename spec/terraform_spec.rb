81249b517fb0ad650139de3c8c8e9d22463edb3e
FGtatsuro <fujiistorage@gmail.com>
2016-08-31 14:31:33 +0900

    Add specs for Terraform.

--- spec/terraform_spec.rb
+++ spec/terraform_spec.rb
@@ -1,17 +1,9 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
-describe package('python-dev'), :if => ['debian', 'alpine'].include?(os[:family]) do
-  it { should be_installed }
-end
-
-describe package('build-essential'), :if => os[:family] == 'debian' do
-  it { should be_installed }
-end
-
-describe package('build-base'), :if => os[:family] == 'alpine' do
-  it { should be_installed }
-end
-
-describe command('which python') do
+describe command('terraform --version') do
   its(:exit_status) { should eq 0 }
+end
+
+describe command('terraform --version'), :if => ['alpine', 'debian'].include?(os[:family]) do
+  its(:stdout) { should contain("Terraform v0.7.2") }
 end
